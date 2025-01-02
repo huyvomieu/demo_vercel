041deebaa2cbff5a595dc1912e3209ac2accf706
Dinh-Linh <linhnd26052004@gmail.com>
2025-01-02 23:59:24 +0700

    last commit

--- routes/api/index.route.js
+++ routes/api/index.route.js
@@ -1,12 +1,11 @@
-const movie = require('./movie.route')
-const user = require('./user.route')
-const banner = require('./banner.route')
-const ticket = require('./ticket.route')
-
+const movie = require("./movie.route");
+const user = require("./user.route");
+const banner = require("./banner.route");
+const ticket = require("./ticket.route");
 
 module.exports = (app) => {
-    app.use("/api/v1/movie", movie)
+    app.use("/api/v1/movie", movie);
     // app.use("/api/v1/user", user)
-    app.use("/api/v1/banner", banner)
-    app.use("/api/v1/ticket", ticket)
-}
+    app.use("/api/v1/banner", banner);
+    app.use("/api/v1/ticket", ticket);
+};
